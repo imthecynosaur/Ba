f11d85de097578f97281c04e1232308488714841
Sina Ghasemi <itsjustsina@gmail.com>
2022-02-08 01:38:45 +0330

    basic ship css added

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,9 +4,11 @@ const screen2 = document.querySelector('.screen-2');
 for (let i = 1; i <= 100; i++) {
     const gridEl = document.createElement('a');
     gridEl.setAttribute('id', `grid-${i}`);
-    gridEl.addEventListener('click', () => {
-        console.log(gridEl);
-    })
+    // gridEl.addEventListener('click', () => {
+    //     const hitEl = document.createElement('div');
+    //     hitEl.classList.add('ship');
+    //     gridEl.appendChild(hitEl);
+    // })
     screen1.append(gridEl);
 }
 
@@ -15,3 +17,14 @@ for (let i = 1; i <= 100; i++) {
     gridEl.setAttribute('id', `grid-${i}`);
     screen2.append(gridEl);
 }
+
+const gridElemnts = document.querySelectorAll('.screen-1 a');
+gridElemnts.forEach((element) => {
+    // console.log(element);
+    element.addEventListener('click', () => {
+       element.classList.toggle('ship'); 
+       element.previousSibling.classList.toggle('ship-start');
+       element.nextSibling.classList.toggle('ship-end');
+    })
+})
+// console.log(gridElemnts);
