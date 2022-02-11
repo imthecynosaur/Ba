8744a55b3173a41dd00627d119cce46fa97b84ad
Sina Ghasemi <itsjustsina@gmail.com>
2022-02-11 19:53:39 +0330

    drag and drop added to arrange section.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,8 @@
 const screen1 = document.querySelector('.screen-1');
 const screen2 = document.querySelector('.screen-2');
 
+
+
 for (let i = 1; i <= 100; i++) {
     const gridEl = document.createElement('a');
     gridEl.setAttribute('id', `grid-${i}`);
@@ -28,3 +30,71 @@ gridElemnts.forEach((element) => {
     })
 })
 // console.log(gridElemnts);
+
+
+
+
+
+
+const map = document.querySelector('.map');
+
+for (let i = 1; i <= 100; i++) {
+    const gridEl = document.createElement('a');
+    gridEl.setAttribute('id', `grid-${i}`);
+
+    gridEl.addEventListener('dragover', event => {
+        event.preventDefault();
+        gridEl.classList.add('highlight');
+    })
+
+    gridEl.addEventListener('dragleave', event => {
+        event.preventDefault();
+        gridEl.classList.remove('highlight');
+    })
+
+    gridEl.addEventListener('drop', event => {
+        event.preventDefault();
+        gridEl.classList.remove('highlight');
+        const ship = document.querySelector(`#ship-${event.dataTransfer.getData('text/plain')}`)
+
+        if (event.dataTransfer.getData('text/plain') == '1') {
+            gridEl.classList.add('ship-start');
+            gridEl.nextSibling.classList.add('ship-end');
+        } else if (event.dataTransfer.getData('text/plain') == '4') {
+            gridEl.classList.add('ship');
+            gridEl.previousSibling.classList.add('ship');
+            gridEl.previousSibling.previousSibling.classList.add('ship-start');
+            gridEl.nextSibling.classList.add('ship-end'); 
+        } else {
+            gridEl.classList.add('ship');
+            gridEl.previousSibling.classList.add('ship-start');
+            gridEl.nextSibling.classList.add('ship-end');
+        }
+
+        ship.style.display = 'none';
+        console.log(event.dataTransfer.getData('text/plain'));
+    })
+
+
+    map.append(gridEl);
+}
+
+
+
+const ships = document.querySelectorAll('.shipp');
+ships.forEach(ship => {
+    ship.addEventListener('dragstart', event => {
+        if (ship.id == 'ship-1') {
+            event.dataTransfer.setData('text/plain', '1');
+        } else if (ship.id == 'ship-2') {
+            event.dataTransfer.setData('text/plain', '2');
+        } else if (ship.id == 'ship-3') {
+            event.dataTransfer.setData('text/plain', '3');
+        } else {
+            event.dataTransfer.setData('text/plain', '4');
+        }
+
+        
+    })
+})
+
