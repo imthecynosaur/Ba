e5bca5ae26931baa5f51c4df3c18bfb033430e60
Sina Ghasemi <itsjustsina@gmail.com>
2022-02-12 01:57:23 +0330

    selecting ships on the map added. control buttons can be toggled on and off now.

--- Scripts/main.js
+++ Scripts/main.js
@@ -51,28 +51,57 @@ for (let i = 1; i <= 100; i++) {
         event.preventDefault();
         gridEl.classList.remove('highlight');
     })
-
+    
     gridEl.addEventListener('drop', event => {
         event.preventDefault();
         gridEl.classList.remove('highlight');
         const ship = document.querySelector(`#ship-${event.dataTransfer.getData('text/plain')}`)
 
         if (event.dataTransfer.getData('text/plain') == '1') {
-            gridEl.classList.add('ship-start');
-            gridEl.nextSibling.classList.add('ship-end');
+            gridEl.classList.add('ship-start', 'number-1');
+            gridEl.nextSibling.classList.add('ship-end', 'number-1');
         } else if (event.dataTransfer.getData('text/plain') == '4') {
-            gridEl.classList.add('ship');
-            gridEl.previousSibling.classList.add('ship');
-            gridEl.previousSibling.previousSibling.classList.add('ship-start');
-            gridEl.nextSibling.classList.add('ship-end'); 
+            gridEl.classList.add('ship', 'number-4');
+            gridEl.previousSibling.classList.add('ship', 'number-4');
+            gridEl.previousSibling.previousSibling.classList.add('ship-start', 'number-4');
+            gridEl.nextSibling.classList.add('ship-end', 'number-4'); 
+        } else if (event.dataTransfer.getData('text/plain') == '3') {
+            gridEl.classList.add('ship', 'number-3');
+            gridEl.previousSibling.classList.add('ship-start', 'number-3');
+            gridEl.nextSibling.classList.add('ship-end', 'number-3');
         } else {
-            gridEl.classList.add('ship');
-            gridEl.previousSibling.classList.add('ship-start');
-            gridEl.nextSibling.classList.add('ship-end');
+            gridEl.classList.add('ship', 'number-2');
+            gridEl.previousSibling.classList.add('ship-start', 'number-2');
+            gridEl.nextSibling.classList.add('ship-end', 'number-2');
         }
 
         ship.style.display = 'none';
         console.log(event.dataTransfer.getData('text/plain'));
+    })
+
+
+
+    gridEl.addEventListener('click', () => {
+        if (gridEl.classList.contains('ship') || gridEl.classList.contains('ship-end') || gridEl.classList.contains('ship-start')){
+            const shipNumber = gridEl.classList[1];
+            console.log(map);
+            const shiplist = map.querySelectorAll(`.${shipNumber}`);
+            shiplist.forEach(ship => {
+                console.log(ship);
+                ship.classList.toggle('ship-highlight');
+            })
+        }
+
+        const rotateBtn = document.querySelector('#rotate-btn');
+        const deleteBtn = document.querySelector('#delete-btn');
+
+        if (map.querySelectorAll('.ship-highlight').length > 0 && map.querySelectorAll('.ship-highlight').length < 5){
+            rotateBtn.classList.add('active');
+            deleteBtn.classList.add('active');
+        } else {
+            rotateBtn.classList.remove('active');
+            deleteBtn.classList.remove('active');
+        }
     })
 
 
