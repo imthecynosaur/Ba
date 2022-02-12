b8bca9783c6a8bdc6f77e798f141fc762b613f7c
Sina Ghasemi <itsjustsina@gmail.com>
2022-02-12 21:52:30 +0330

    delete button is now working

--- Scripts/main.js
+++ Scripts/main.js
@@ -22,18 +22,49 @@ for (let i = 1; i <= 100; i++) {
 
 const gridElemnts = document.querySelectorAll('.screen-1 a');
 gridElemnts.forEach((element) => {
-    // console.log(element);
     element.addEventListener('click', () => {
        element.classList.toggle('ship'); 
        element.previousSibling.classList.toggle('ship-start');
        element.nextSibling.classList.toggle('ship-end');
     })
 })
-// console.log(gridElemnts);
 
 
 
 
+
+
+
+
+
+
+/*---------------------------------------------------------------------*/
+
+
+const rotateFunc = function() {
+    console.log('rotated');
+}
+
+const deleteFunc = function() {
+    console.log('deleted');
+    
+    const shipOnMap = map.querySelectorAll('.ship-highlight');
+    const shipId = shipOnMap[0].classList[1];
+    document.querySelector(`#${shipId}`).style.display = 'block';
+    shipOnMap.forEach(ship => {
+        ship.removeAttribute('class');
+    })
+}
+
+
+
+
+
+
+
+
+const rotateBtn = document.querySelector('#rotate-btn');
+const deleteBtn = document.querySelector('#delete-btn');
 
 
 const map = document.querySelector('.map');
@@ -55,7 +86,7 @@ for (let i = 1; i <= 100; i++) {
     gridEl.addEventListener('drop', event => {
         event.preventDefault();
         gridEl.classList.remove('highlight');
-        const ship = document.querySelector(`#ship-${event.dataTransfer.getData('text/plain')}`)
+        const ship = document.querySelector(`#number-${event.dataTransfer.getData('text/plain')}`)
 
         if (event.dataTransfer.getData('text/plain') == '1') {
             gridEl.classList.add('ship-start', 'number-1');
@@ -76,7 +107,6 @@ for (let i = 1; i <= 100; i++) {
         }
 
         ship.style.display = 'none';
-        console.log(event.dataTransfer.getData('text/plain'));
     })
 
 
@@ -84,23 +114,24 @@ for (let i = 1; i <= 100; i++) {
     gridEl.addEventListener('click', () => {
         if (gridEl.classList.contains('ship') || gridEl.classList.contains('ship-end') || gridEl.classList.contains('ship-start')){
             const shipNumber = gridEl.classList[1];
-            console.log(map);
             const shiplist = map.querySelectorAll(`.${shipNumber}`);
             shiplist.forEach(ship => {
-                console.log(ship);
                 ship.classList.toggle('ship-highlight');
             })
         }
 
-        const rotateBtn = document.querySelector('#rotate-btn');
-        const deleteBtn = document.querySelector('#delete-btn');
-
         if (map.querySelectorAll('.ship-highlight').length > 0 && map.querySelectorAll('.ship-highlight').length < 5){
             rotateBtn.classList.add('active');
             deleteBtn.classList.add('active');
+
+            rotateBtn.addEventListener('click', rotateFunc)
+            deleteBtn.addEventListener('click', deleteFunc)
         } else {
             rotateBtn.classList.remove('active');
             deleteBtn.classList.remove('active');
+
+            rotateBtn.removeEventListener('click', rotateFunc);
+            deleteBtn.removeEventListener('click', deleteFunc);
         }
     })
 
@@ -113,11 +144,11 @@ for (let i = 1; i <= 100; i++) {
 const ships = document.querySelectorAll('.shipp');
 ships.forEach(ship => {
     ship.addEventListener('dragstart', event => {
-        if (ship.id == 'ship-1') {
+        if (ship.id == 'number-1') {
             event.dataTransfer.setData('text/plain', '1');
-        } else if (ship.id == 'ship-2') {
+        } else if (ship.id == 'number-2') {
             event.dataTransfer.setData('text/plain', '2');
-        } else if (ship.id == 'ship-3') {
+        } else if (ship.id == 'number-3') {
             event.dataTransfer.setData('text/plain', '3');
         } else {
             event.dataTransfer.setData('text/plain', '4');
