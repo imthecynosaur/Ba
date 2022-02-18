4f70a8a58d0e1dca191d495615b70549e9ca46c2
Sina Ghasemi <itsjustsina@gmail.com>
2022-02-19 02:27:18 +0330

    Map and Ship classes updated.

--- Scripts/arrangePhase.js
+++ Scripts/arrangePhase.js
@@ -1,16 +1,21 @@
 import { Map } from './map.js';
 
-
-// const arrangePhase = document.querySelector('template')
 const arrangePhase = document.importNode(document.querySelector('template').content, true);
-console.log(arrangePhase);
 const body = document.querySelector('body');
 body.prepend(arrangePhase);
 
-const mapElement = document.querySelectorAll('.map');
-// console.log(MapElement);
-mapElement.forEach(map => {
-    new Map(map);
-})
-// const map = new Map(mapElement);
+const mapElement = document.querySelector('.map');
+const map = new Map(mapElement);
 
+
+const shipElements = document.querySelectorAll('.shipp');
+shipElements.forEach(shipElement => {
+    shipElement.addEventListener('dragstart', () => {
+        map.setCurrentShip(shipElement);
+    });
+
+    shipElement.addEventListener('dblclick', (event) => {
+        console.log('db clicked');
+        event.target.classList.toggle('horizontal');
+    })
+})
--- Scripts/map.js
+++ Scripts/map.js
@@ -1,8 +1,11 @@
+import {Ship} from './ship.js';
+
 export class Map {
     constructor(hostElement){
         this.hostElement = hostElement;
-        this.gridArray = [];
+        this.ships = [];
         this.createGrid();
+        this.createDropZone();
     }
 
     createGrid(){
@@ -13,8 +16,56 @@ export class Map {
         }
     }
 
-    renderShip(){
+    createDropZone(){
+        this.hostElement.addEventListener('dragover', event => {
+            event.preventDefault();
+            event.target.classList.add('highlight');
+        })
+
+        this.hostElement.addEventListener('dragleave', event => {
+            event.preventDefault();
+            event.target.classList.remove('highlight');
+        })
+        
+        this.hostElement.addEventListener('drop', event => {
+            event.preventDefault();
+            event.target.classList.remove('highlight');
+            if (this.createShip(this.currentShipId, this.currentShipDirection, this.currentShipLenght, +event.target.id.split("-")[1])){
+                this.currentShip.style.display = 'none';
+            }
+        })
+    }
+    
+    setCurrentShip(ship) {
+        this.currentShip = ship;
+        this.currentShipId = ship.id;
+        this.currentShipDirection = ship.classList.contains('horizontal') ? 'horizontal' : 'vertical';
+        this.currentShipLenght = ship.id.split("-")[1] == '1' ? 2 : ship.id.split("-")[1] == '4' ? 4 : 3;
     }
 
+    createShip(shipId, shipDirection, shipLenght, gridId){
+        const newShip = new Ship(shipLenght, shipDirection, shipId)
+        this.ships.push(newShip);
+        if(newShip.setShipPosition(gridId) == false){
+            this.ships.pop();
+            return false;
+        }
+        for(const ship of this.ships){
+            if(ship.id != newShip.id){
+                for (const position of ship.position) {
+                    for (const Position of newShip.position) {
+                        if (position == Position){
+                            console.log('shit');
+                            this.ships.pop();
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        newShip.render(this.hostElement);
+        return true;
+    }
 
+    
 }
